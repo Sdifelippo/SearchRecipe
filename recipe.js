31cd5ee17a29373ba5ddcd5d0f2e7986a9cc05f1
Steven Difelippo <matrixdriver04@gmail.com>
2017-07-19 20:44:04 -0400

    completerecipesearch

--- recipe.js
+++ recipe.js
@@ -1,22 +1,43 @@
+let searchbar = document.querySelector("#recipe");
+let button = document.querySelector(".button");
 let container = document.querySelector(".container");
-let search = document.querySelector(".searchbar");
 
 
-fetch("https://crossorigin.me/http://www.recipepuppy.com/api/?q=" + (recipe.search.value))
+button.addEventListener("click", function search() {
+  container.innerHTML = "";
+  fetch("https://crossorigin.me/http://www.recipepuppy.com/api/?q=" + searchbar.value)
+    .then(function(response) {
 
-    .then(function(obj) {
+      if (response.status !== 200) {
+        console.log(response.status);
         return;
+      }
+      response.json().then(function(obj) {
+
+
+        obj.results.forEach(function(result) {
+
+          let imagesource = result.thumbnail
+          let title = result.title
+          let link = result.href
+
+          result = `
+            <div class="wrapper">
+          <h3>${title}<h3>
+                         <br>
+                         <a href="${link}">
+                         <img src="${imagesource}" onError="this.src='dinner plate.png'"></a>
+                         </div>`; 
+
+
+
+          container.innerHTML += result;
+
+        });
+      })
+
     })
-    response.json().then(function(object) {
-        console.log(object.title);
-          console.log(object.results);
-
-      info.innerHTML += `
-        <h2>The Info<h2>
-          ${results.title}
-            `
-  })
-  .then(function(obj){
-    console.log(obj)
-
-  });
+    .catch(function(error) {
+      console.log('Fetch Error :-S', err);
+    });
+})
